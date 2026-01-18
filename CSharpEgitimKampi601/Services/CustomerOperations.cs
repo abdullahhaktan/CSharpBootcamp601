0d697af2f1e2cd9f2bf6c2653400eb27a951df71
Abdullah Haktan <abdullahhaktan9@gmail.com>
2026-01-18 18:53:17 +0300

    Update CustomerOperations.cs

--- CSharpEgitimKampi601/Services/CustomerOperations.cs
+++ CSharpEgitimKampi601/Services/CustomerOperations.cs
@@ -1,4 +1,4 @@
-﻿using CSharpEgitimKampi601.Entities;
+using CSharpEgitimKampi601.Entities;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
@@ -11,27 +11,36 @@ namespace CSharpEgitimKampi601.Services
 {
     public class CustomerOperations
     {
+        // Add a new customer to MongoDB
         public void AddCustomer(Customer customer)
         {
             var connection = new MongoDbConnection();
             var customerCollection = connection.GetCustomersCollection();
 
+            // Create BSON document from Customer object
             var document = new BsonDocument
             {
-                {"CustomerName",customer.CustomerName },
-                {"CustomerSurname",customer.CustomerSurname },
-                {"CustomerCity",customer.CustomerCity },
-                {"CustomerBalance",customer.CustomerBalance },
-                {"CustomerShoppingCount",customer.CustomerShoppingCount }
+                {"CustomerName", customer.CustomerName },
+                {"CustomerSurname", customer.CustomerSurname },
+                {"CustomerCity", customer.CustomerCity },
+                {"CustomerBalance", customer.CustomerBalance },
+                {"CustomerShoppingCount", customer.CustomerShoppingCount }
             };
 
+            // Insert document into MongoDB collection
             customerCollection.InsertOne(document);
         }
+        
+        // Retrieve all customers from MongoDB
         public List<Customer> GetAllCustomer()
         {
             var connection = new MongoDbConnection();
             var customerCollection = connection.GetCustomersCollection();
+            
+            // Find all documents in the collection
             var customers = customerCollection.Find(new BsonDocument()).ToList();
+            
+            // Convert BSON documents to Customer objects
             List<Customer> customerList = new List<Customer>();
             foreach (var c in customers)
             {
@@ -47,32 +56,54 @@ namespace CSharpEgitimKampi601.Services
             }
             return customerList;
         }
+        
+        // Delete customer by ID from MongoDB
         public void DeleteCustomer(string id)
         {
             var connection = new MongoDbConnection();
             var customerCollection = connection.GetCustomersCollection();
+            
+            // Create filter to find document by ID
             var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+            
+            // Delete the document
             customerCollection.DeleteOne(filter);
         }
+        
+        // Update existing customer in MongoDB
         public void UpdateCustomer(Customer customer)
         {
             var connection = new MongoDbConnection();
             var customerCollection = connection.GetCustomersCollection();
+            
+            // Create filter to find document by ID
             var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(customer.CustomerId));
+            
+            // Create update definition with all fields
             var updatedValue = Builders<BsonDocument>.Update
                 .Set("CustomerName", customer.CustomerName)
                 .Set("CustomerSurname", customer.CustomerSurname)
                 .Set("CustomerCity", customer.CustomerCity)
                 .Set("CustomerBalance", customer.CustomerBalance)
                 .Set("CustomerShoppingCount", customer.CustomerShoppingCount);
+            
+            // Update the document
             customerCollection.UpdateOne(filter, updatedValue);
         }
+        
+        // Get single customer by ID from MongoDB
         public Customer GetCustomerById(string id)
         {
             var connection = new MongoDbConnection();
             var customerCollection = connection.GetCustomersCollection();
+            
+            // Create filter to find document by ID
             var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+            
+            // Find the document
             var result = customerCollection.Find(filter).FirstOrDefault();
+            
+            // Convert BSON document to Customer object
             return new Customer
             {
                 CustomerBalance = decimal.Parse(result["CustomerBalance"].ToString()),
