b213048c3343df188390aaf7c0ccca8abe62bba7
Abdullah Haktan <abdullahhaktan9@gmail.com>
2026-01-18 18:53:47 +0300

    Enhance MongoDbConnection with comments
    Added comments to clarify connection setup and collection retrieval.

--- CSharpEgitimKampi601/Services/MongoDbConnection.cs
+++ CSharpEgitimKampi601/Services/MongoDbConnection.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Bson;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -11,12 +11,18 @@ namespace CSharpEgitimKampi601.Services
     public class MongoDbConnection
     {
         private IMongoDatabase _database;
+        
+        // Constructor establishes connection to MongoDB
         public MongoDbConnection()
         {
+            // Connect to MongoDB running on localhost default port
             var client = new MongoClient("mongodb://localhost:27017");
+            
+            // Get reference to specific database
             _database = client.GetDatabase("Db601Customer");
         }
 
+        // Get collection for customer documents
         public IMongoCollection<BsonDocument> GetCustomersCollection()
         {
             return _database.GetCollection<BsonDocument>("Customers");
